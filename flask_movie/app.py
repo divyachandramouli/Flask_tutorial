7501f07c7cd9983e31d7ddc861352a9549901147
Divya_Chandramouli <cdivya.mail@gmail.com>
2018-07-17 17:35:29 -0700

    Defined a User table

--- flask_movie/app.py
+++ flask_movie/app.py
@@ -8,10 +8,26 @@ app=Flask(__name__)
 #Set a key value to the config string for your database
 #mysql://username:password@server/db
 
+# Connect the app with the database
 app.config['SQLALCHEMY_DATABASE_URI']="postgres://postgres:cd123@localhost/flaskmovie"
 
 # Create database object , pass app as arg
 db=SQLALchemy(app)
+
+# Create database tables/schema
+# A table for a user
+class User(db.Model):
+	id=db.Column(db.Integer, primary_key=True) #This will autoincrement for every user added
+	username=db.Column(db.String(80), unique=True, nullable=False)
+	email=db.Column(db.String(120), unique=True, nullable=False)
+
+	def __init__(self,username,email):
+		self.username=username
+		self.email=email
+
+	def __repr__(self):
+		return '<User %r>' % self.username
+
 
 @app.route('/')
 def index():
